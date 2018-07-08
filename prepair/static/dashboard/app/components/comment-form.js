ddfc47715cf3b07636b9b7b637e33c3d7e960ddc
bfolks2 <benfolks@Bens-MacBook-Pro.local>
2018-07-08 18:35:14 -0500

    new components for each post and comment, to allow for easier management

--- prepair/static/dashboard/app/components/comment-form.js
+++ prepair/static/dashboard/app/components/comment-form.js
@@ -3,12 +3,12 @@ import Component from '@ember/component';
 export default Component.extend({
 
   /**
-   * The post bound to this component.
+   * The comment bound to this component.
    *
    * @argument post
    * @type object
    * @default null
    */
-  post: null,
+  comment: null,
 
 });
